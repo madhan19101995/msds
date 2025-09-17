8223d869ce6e0eb2331353ca5fa52c1a14f11a0e
madhan19101995 <madhan.mahe@gmail.com>
2025-09-17 15:17:39 +0300

    Update

--- script.js
+++ script.js
@@ -125,6 +125,7 @@ const documents = [
     { name: "Contact Adhesives_MSDS", file: "documents/Contact Adhesives.pdf" },
     { name: "Dura-Tile-Grout MSDS_MSDS", file: "documents/Dura-Tile-Grout MSDS.pdf" },
     { name: "Marmolit-Solid-Straw-SDS_MSDS", file: "documents/Marmolit-Solid-Straw-SDS.pdf" },
+    { name: "Paint-thinner_MSDS", file: "documents/Paint-thinner.pdf" },
     { name: "CMHS CHEMICAL INVENTORY 2nd Week SEP 2025_MSDS", file: "documents/CMHS CHEMICAL INVENTORY 2nd Week SEP 2025.pdf" }
 ]; 
 
