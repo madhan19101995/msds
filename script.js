965791d3a1bf42c231bf582fc2815fbe3d6e04a5
madhan19101995 <madhan.mahe@gmail.com>
2025-09-29 16:40:25 +0300

    update

--- script.js
+++ script.js
@@ -135,7 +135,7 @@ const documents = [
     { name: "JOTUN Penguard Universal Comp A_MSDS", file: "documents/JOTUN Penguard Universal Comp A.pdf" },
     { name: "JOTUN Penguard Universal Comp B_MSDS", file: "documents/JOTUN Penguard Universal Comp B.pdf" },
     { name: "Coolant sds_MSDS", file: "documents/Coolant sds.pdf" },
-    { name: "CMHS CHEMICAL INVENTORY 2nd Week SEP 2025_MSDS", file: "documents/CMHS CHEMICAL INVENTORY 2nd Week SEP 2025.pdf" }
+    { name: "CMHS CHEMICAL INVENTORY 3rd Week SEP 2025_MSDS", file: "documents/CMHS CHEMICAL INVENTORY 3rd Week SEP 2025.pdf" }
 ]; 
 
 documents.sort((a, b) => a.name.localeCompare(b.name));
